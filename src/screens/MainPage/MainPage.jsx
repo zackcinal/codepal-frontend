e2b3de25bd359789ec88cfc8dbbccaf6b1a9cdae
Cesar Iparrea <cesariparrea@hotmail.com>
2024-04-10 09:54:47 -0400

    Main Screen

--- src/screens/MainPage/MainPage.jsx
+++ src/screens/MainPage/MainPage.jsx
@@ -3,7 +3,32 @@ import "./MainPage.css"
 
 function MainPage() {
   return (
-    <div className='mainPageContainer'>MainPage</div>
+    <div className='mainPageContainer'>
+      <div className='mainPageProfileContainer'>
+
+
+      </div>
+      <div className='mainPageScreen'>
+        <div className='mainPageOptions'>
+          <button className='mainPageOptionsBtn'>Projects</button>
+          <button className='mainPageOptionsBtn'>Reviews</button>
+          <button className='mainPageOptionsBtn'>Followers</button>
+          <button className='mainPageOptionsBtn'>Following</button>
+        </div>
+        <div className='mainPageDisplay'>
+          <div className='review'>Review</div>
+          <div className='review'>Review</div>
+          <div className='review'>Review</div>
+          <div className='review'>Review</div>
+          <div className='review'>Review</div>
+          <div className='review'>Review</div>
+          <div className='review'>Review</div>
+        </div>
+      </div>
+
+
+
+    </div>
   )
 }
 

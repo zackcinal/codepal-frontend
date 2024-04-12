74af77642fbab87211767ff8dac1219a493e1dac
Antonio <afelixj89@gmail.com>
2024-04-12 18:47:34 -0400

    CSS the Sign up form(inputs giving diferent styling to give it a more refreshing look + made the inernal scroll bar invisible)...still needs media querys

--- src/screens/SignUp/SignUp.jsx
+++ src/screens/SignUp/SignUp.jsx
@@ -80,107 +80,115 @@ const Register = (props) => {
       <div className="signUpFormContainer">
         <h1 className="signUpHeader">Sign Up</h1>
         <form className="signupForm" onSubmit={handleSubmit}>
+          <label htmlFor="first_name" className="signupInputLabel">First Name</label>
           <input
             type="text"
             name="first_name"
+            id="first_name"
             value={form.first_name}
-            placeholder="Enter Your First Name"
             onChange={handleChange}
             required
             autoComplete="off"
             className="signupInput"
+            style={{ textTransform: 'capitalize' }}
           />
+          <label htmlFor="last_name" className="signupInputLabel">Last Name</label>
           <input
             type="text"
             name="last_name"
+            id="last_name"
             value={form.last_name}
-            placeholder="Enter Your Last Name"
             onChange={handleChange}
             required
             autoComplete="off"
             className="signupInput"
+            style={{ textTransform: 'capitalize' }}
           />
+          <label htmlFor="username" className="signupInputLabel">Username</label>
           <input
             type="text"
             name="username"
+            id="username"
             value={form.username}
-            placeholder="Enter Username"
             onChange={handleChange}
-            required
             autoComplete="off"
             className="signupInput"
           />
+          <label htmlFor="email" className="signupInputLabel">Email</label>
           <input
             type="email"
             name="email"
+            id="email"
             value={form.email}
-            placeholder="Enter Email"
             onChange={handleChange}
             required
             autoComplete="off"
             className="signupInput"
           />
+          <label htmlFor="password" className="signupInputLabel">Password</label>
           <input
             type="password"
             name="password"
+            id="password"
             value={form.password}
-            placeholder="Enter Password"
             onChange={handleChange}
             required
             autoComplete="off"
             className="signupInput"
           />
+          <label htmlFor="confirmPassword" className="signupInputLabel">Confirm Password</label>
           <input
             type="password"
             name="confirmPassword"
+            id="confirmPassword"
             value={form.confirmPassword}
-            placeholder="Confirm Password"
             onChange={handleChange}
             required
             autoComplete="off"
             className="signupInput"
           />
+          <label htmlFor="profile_picture" className="signupInputLabel">Profile Picture</label>
           <input
             type="file"
             name="profile_picture"
             accept="image/jpeg,image/png,image/gif"
-            placeholder="Upload a profile picture"
             onChange={handleFileChange}
-            required
             autoComplete="off"
             className="signupInput"
           />
+          <label htmlFor="description" className="signupInputLabel">Description</label>
           <input
             type="text"
             name="description"
             value={form.description}
-            placeholder="Enter Description"
+
             onChange={handleChange}
             required
             autoComplete="off"
             className="signupInput"
-            id="fileInput"
           />
+          <label htmlFor="location" className="signupInputLabel">Location</label>
           <input
             type="text"
             name="location"
             value={form.location}
-            placeholder="Enter Location"
             onChange={handleChange}
             required
             autoComplete="off"
             className="signupInput"
           />
+          <label htmlFor="portfolio_link" className="signupInputLabel">Portfolio Link</label>
           <input
             type="text"
             name="portfolio_link"
             value={form.portfolio_link}
-            placeholder="Enter Portfolio Link"
+          
             onChange={handleChange}
             required
             autoComplete="off"
             className="signupInput"
           />
+          <label htmlFor="role" className="signupInputLabel">Select a role</label>
           <select
             name="role"
             value={form.role}
@@ -194,7 +202,7 @@ const Register = (props) => {
             <option value="FE">Front End Developer</option>
             <option value="UX">User Experience Designer</option>
           </select>
-
+  
           <div className="signUpCheckbox">
             <p>Are you a developer?</p>
             <input
@@ -210,6 +218,7 @@ const Register = (props) => {
       </div>
     </div>
   );
-};
-
+  
+  
+}
 export default Register;

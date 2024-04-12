29d92283b0d5d3d6dc79fcfca4d5deb17caebc8d
arehmanlatif1 <rehmanlatif335@gmail.com>
2024-04-12 14:42:41 -0400

    the main page is working with the filter

--- src/screens/MainPage/MainPage.jsx
+++ src/screens/MainPage/MainPage.jsx
@@ -1,35 +1,114 @@
-import React, { useEffect, useState } from 'react'
-import "./MainPage.css"
-import Footer from '../../components/Footer/Footer'
-import axios from 'axios'
-import UserPreview from '../../components/UserPreview/UserPreview'
-import { getProfiles, getProfileUser } from '../../services/users'
-
+import React, { useEffect, useState } from "react";
+import "./MainPage.css";
+import Footer from "../../components/Footer/Footer";
+import axios from "axios";
+import UserPreview from "../../components/UserPreview/UserPreview";
+import { getProfiles, getProfileUser } from "../../services/users";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
 function MainPage() {
-  const [profiles, setProfiles] = useState([])
+  const [profiles, setProfiles] = useState([]);
 
   useEffect(() => {
-    async function fetchProfiles(){
-      let response = await getProfiles()
-      setProfiles(response)
-      console.log(response)
+    async function fetchProfiles() {
+      let response = await getProfiles();
+      console.log("tab");
+      setProfiles(response);
     }
 
-    fetchProfiles()
+    fetchProfiles();
   }, []);
 
   return (
-    <div className='mainPageContainer'>
-      <h1>Developers</h1>
-      <div className='profilesContainer'>
-        {profiles.map(profile => (
-          <UserPreview profile={profile} key={profile.id} />
-        ))}
-      </div>
+    <div className="mainPageContainer">
+      <Tabs className="mainPageScreen">
+        <TabList className="mainPageOptions">
+          <Tab>
+            <p className="mainPageOptionsBtn">All Developers</p>
+          </Tab>
+          <Tab>
+            <p className="mainPageOptionsBtn">Full Stack</p>
+          </Tab>
+          <Tab>
+            <p className="mainPageOptionsBtn">Front End</p>
+          </Tab>
+          <Tab>
+            <p className="mainPageOptionsBtn">Back End</p>
+          </Tab>
+          <Tab>
+            <p className="mainPageOptionsBtn">User Experience</p>
+          </Tab>
+        </TabList>
+
+        <TabPanel>
+          <div className="mainPageDisplay">
+            <h1>Developers</h1>
+            <div className="profilesContainer">
+              {profiles.map((profile) => (
+                <UserPreview profile={profile} key={profile.id} />
+              ))}
+            </div>
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="mainPageDisplay">
+            <h1>Full Stack</h1>
+            <div className="profilesContainer">
+              {profiles.map((profile) =>
+                profile.role === "FS" ? (
+                  <UserPreview profile={profile} key={profile.id} />
+                ) : null
+              )}
+            </div>
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="mainPageDisplay">
+            <h1>Front End</h1>
+            <div className="profilesContainer">
+              {profiles.map((profile) =>
+                profile.role === "FE" ? (
+                  <UserPreview profile={profile} key={profile.id} />
+                ) : null
+
+                // if(profile.role === "FE"){
+                //   <UserPreview profile={profile} key={profile.id} />
+                // }else{
+                //   ""
+                // }
+              )}
+            </div>
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="mainPageDisplay">
+            <h1>Back End</h1>
+            <div className="profilesContainer">
+              {profiles.map((profile) =>
+                profile.role === "BE" ? (
+                  <UserPreview profile={profile} key={profile.id} />
+                ) : null
+              )}
+            </div>
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="mainPageDisplay">
+            <h1>User Experience</h1>
+            <div className="profilesContainer">
+              {profiles.map((profile) =>
+                profile.role === "UX" ? (
+                  <UserPreview profile={profile} key={profile.id} />
+                ) : null
+              )}
+            </div>
+          </div>
+        </TabPanel>
+      </Tabs>
+
       <Footer />
     </div>
-  )
+  );
 }
 
-export default MainPage
+export default MainPage;
